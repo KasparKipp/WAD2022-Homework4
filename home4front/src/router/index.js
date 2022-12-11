05badddacf1c20f5d0b183e5bbe38b69703c1e0e
Kaspar <kaspar.kipp@gmail.com>
2022-12-11 19:15:06 +0200

    Added view and route for not defined routes

--- home4front/src/router/index.js
+++ home4front/src/router/index.js
@@ -67,6 +67,14 @@ const routes = [
 		// which is lazy-loaded when the route is visited.
 		component: () => import(/* webpackChunkName: "about" */ "../views/ContactsView.vue"),
 	},
+	{
+		path: "/:anythingbutaslash([^/]+)",
+		name: "Lost",
+		// route level code-splitting
+		// this generates a separate chunk (about.[hash].js) for this route
+		// which is lazy-loaded when the route is visited.
+		component: () => import(/* webpackChunkName: "about" */ "../views/LostView.vue"),
+	},
 ];
 
 const router = createRouter({
---
+++ home4front/src/views/LostView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="lost">
+    <h1>You are lost dear traveller</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LostView",
+}
+</script>
